077cbb6a3df93f898134d49c04dff70b5f5bd779
Pol Zeimet <pol.zeimet@gmail.com>
2017-12-24 13:17:34 +0100

    stuff for fa, bootstrap, sass, etc....

--- conspec/main.js
+++ conspec/main.js
@@ -9,15 +9,19 @@ function createWindow () {
     icon: `file://${__dirname}/dist/assets/logo.png`
   })
   win.loadURL(`file://${__dirname}/dist/index.html`)
+  
   //// uncomment below to open the DevTools.
-  // win.webContents.openDevTools()
+  win.webContents.openDevTools()
+  
   // Event when the window is closed.
   win.on('closed', function () {
     win = null
   })
 }
+
 // Create window on electron intialization
 app.on('ready', createWindow)
+
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
   // On macOS specific close process
